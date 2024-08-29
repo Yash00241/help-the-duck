924c99e2a193a3a99a596a4d66334e70c1d7f2ea
Yash Vardhan Niranjan <83543243+Yash00241@users.noreply.github.com>
2024-08-29 16:37:12 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,8 +1,106 @@
-const playerChar = "p";
-const wallChar = "1";
-const backgroundChar = ".";
+/*
+First time? Check out the tutorial game:
+https://sprig.hackclub.com/gallery/getting_started
 
-setMap(map`
+@title: Dan The Bird
+@author: Yash00241
+@tags: ['begginer', 'endless', 'duck', 'flappybird']
+@addedOn: 2024-08-29
+*/
+var bgm = tune`
+200: A4^200 + C5^200 + E5^200 + A5^200,
+200: G4~200 + C5~200 + E5-200 + D5^200,
+200: F4^200 + A4~200 + C5^200,
+200: E4~200 + G4~200 + B4-200 + C5^200,
+200: D5^200 + A4^200 + F5~200 + E5-200,
+200: C5^200 + E4~200 + G4-200 + D5^200,
+200: B4~200 + G4^200 + E5-200 + C5~200,
+200: A4^200 + F5~200 + D5^200 + G4^200,
+200: E5^200 + C5~200 + A4-200 + B4~200,
+200: F4^200 + D4^200 + E5~200 + G4^200,
+200: C5^200 + G4~200 + E4-200 + D5^200,
+200: A4^200 + C5^200 + B4~200 + E5-200,
+200: G4^200 + D5^200 + C5^200 + F5~200,
+200: E4~200 + G4~200 + A4^200 + C5~200,
+200: B4-200 + F5^200 + G4~200 + E5~200,
+200: D5^200 + C5~200 + A4~200 + G4^200,
+200: E5~200 + C5^200 + F4~200 + B4-200,
+200: G4^200 + D5^200 + A4~200 + C5~200,
+200: A4^200 + C5^200 + E5^200 + A5^200,
+200: G4~200 + C5~200 + E5-200 + D5^200,
+200: F4^200 + A4~200 + C5^200,
+200: E4~200 + G4~200 + B4-200 + C5^200,
+200: D5^200 + A4^200 + F5~200 + E5-200,
+200: C5^200 + E4~200 + G4-200 + D5^200,
+200: B4~200 + G4^200 + E5-200 + C5~200,
+200: A4^200 + F5~200 + D5^200 + G4^200,
+200: E5^200 + C5~200 + A4-200 + B4~200,
+200: F4^200 + D4^200 + E5~200 + G4^200,
+200: C5^200 + G4~200 + E4-200 + D5^200,
+200: A4^200 + C5^200 + B4~200 + E5-200,
+200: G4^200 + D5^200 + C5^200 + F5~200,
+200: E4~200 + G4~200 + A4^200 + C5~200`
+var music = playTune(bgm, Infinity);
+
+const player = "p";
+const wall = "w";
+const background = "b";
+
+setLegend(
+  [ player, bitmap`
+................
+................
+................
+................
+..........000000
+......000.066660
+.....00990060060
+000..09999066060
+0660000090066660
+0066666000666660
+.066666666666600
+.06666666666660.
+.00666666666660.
+..0066666666660.
+...006666666600.
+.....000000000..`],
+  [ wall, bitmap`
+DDDDDDDDDDDDDDDD
+D4444D444444444D
+DDD44D444444444D
+D4444D4DD444444D
+D4444D444444444D
+DDDDDDDDDDDDDDDD
+D4444444444D444D
+D44444DD444D44DD
+D4444444444D44DD
+D4444444444D444D
+DDDDDDDDDDDDDDDD
+D444444D4444444D
+D4DD444D4444444D
+D444444D4444444D
+D444444D4444DD4D
+DDDDDDDDDDDDDDDD`],
+  [ background, bitmap`
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000` ]
+);
+
+setMap( map`
 .......w
 .......w
 .......w
@@ -11,38 +109,34 @@ setMap(map`
 .......w
 .......w
 .......w` );
-setBackground(backgroundChar);
+setBackground(background);
 
-let opening = 3;
-let speed = 250;
-let score = 0;
-let isGameOver = false;
+var opening = 3;
+var speed = 250;
+var score = 0;
+var isGameOver = false;
 
 setPushables({
-  [playerChar]: [],
+  [ player ]: [],
 });
 
 onInput("s", () => {
   if (!isGameOver) {
-    const player = getFirst(playerChar);
-    player.y += 1;
-    if (player.y > 7) player.y = 7; 
+    getFirst(player).y += 1
   }
 });
 
 onInput("w", () => {
   if (!isGameOver) {
-    const player = getFirst(playerChar);
-    player.y -= 1;
-    if (player.y < 0) player.y = 0; 
+    getFirst(player).y -= 1
   }
 });
 
 function genWall() {
   opening = Math.floor(Math.random() * 8);
-  for (let y = 0; y < 8; y++) {
-    if (y !== opening) {
-      addSprite(7, y, wallChar);
+  for (let y=0; y < 8; y++) {
+    if (y != opening) {
+      addSprite(7, y, wall);
     }
   }
 
@@ -50,29 +144,25 @@ function genWall() {
 }
 
 function gameLoop() {
-  addText(`Score: ${score}`, {x: 9, y: 14, color: color`F`});
+  addText(`Score: ${score}`, {x: 9, y: 14,color: color`7`})
     
-  getAll(wallChar).forEach((w) => {
-    if (w.x === 0) {
+  getAll(wall).forEach((w) => {
+    if (w.x == 0) {
       w.remove();
     } else {
       w.x -= 1;
-    }
+    };
   });
 
-  if (getAll(wallChar).length === 0) {
+  if (getAll(wall).length == 0) {
     genWall();
   }
 
-  const player = getFirst(playerChar);
-  const walls = getAll(wallChar);
-  const collision = walls.some(w => w.x === player.x && w.y !== opening);
-  
-  if (collision) {
-    lost();
+  if (getFirst(wall).x == getFirst(player).x && getFirst(player).y != opening) {
+      lost();
   } 
 
-  speed = Math.max(100, speed - (250 - speed)); 
+  speed -= (250-speed);
   if (!isGameOver) {
     setTimeout(gameLoop, speed);
   }
@@ -90,8 +180,8 @@ function lost() {
 ........
 ........`);
   clearText();
-  addText("Game over!", {x: 5, y: 7, color: color`C`});
-  addText(`Score: ${score}`, {x: 5, y: 8, color: color`4`});
+  addText("Game over!", {x: 5, y: 7, color: color`2`})
+  addText(`Score: ${score}`, {x: 5, y: 8, color: color`7`})
 }
 
 gameLoop();
